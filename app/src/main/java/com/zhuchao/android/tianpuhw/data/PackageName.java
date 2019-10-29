e18f640e09c16383aa9e637ed9f838506cca67a6
octopus-okan <m@1234998.cn>
2019-10-29 23:24:48 +0800

    Initial commit

---
+++ app/src/main/java/com/zhuchao/android/tianpuhw/data/PackageName.java
@@ -0,0 +1,14 @@
+package com.zhuchao.android.tianpuhw.data;
+
+/**
+ * 一些app的包名
+ */
+public interface PackageName {
+    //全民K歌
+    String qmSing = "com.tencent.karaoketv";
+    String qqMusic = "com.tencent.qqmusictv";
+    String qqTv = "com.ktcp.tvvideo";
+    String hdp = "hdpfans.com";
+
+    String dbMarket = "com.dangbeimarket";
+}
